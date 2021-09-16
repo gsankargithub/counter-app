71e5cb3ceeca01ccc3ec764036d2683318fca8c9
Sankar <gsankarsvga@gmail.com>
2021-09-16 06:48:11 +0530

    Learned upto setState

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,21 +3,44 @@ import React, { Component } from 'react';
 
 class Counter extends React.Component {
     state = {
-        count: 0
+        count: 0,
+        tags: ['tag1', 'tag2', 'tag3']
     };
+
+    // constructor() {
+    //     super();
+    //     this.handleIncrement = this.handleIncrement.bind(this);
+    // }
+
+    handleIncrement =() =>  {
+        console.log("Increment Clicked",this); 
+      
+        this.setState({count : this.state.count+1 })
+    }
+
     render() {
         return (
             <div>
-                <span className = "badge badge-primary m-2">{this.formatCount()}</span>
-                <button className = "btn btn-secondary btn-sm">
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">
                     Increment
                 </button>
+                <ul>
+                    {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
+                </ul>
             </div>
         );
     }
+    getBadgeClasses() {
+        let classes = "badge m-2 ";
+        classes += this.state.count === 0 ? "badge-warning" : "badge-primary";
+        return classes;
+    }
+
     formatCount() {
         return this.state.count === 0 ? 'Zero' : this.state.count;
     }
+
 }
 
 export default Counter;
